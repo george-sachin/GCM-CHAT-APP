730810832c4477f6ec5ce5219c8de680c455eb7a
Vanessa96 <vanessa.almond28@gmail.com>
2019-05-04 17:44:52 +0530

    Initial commit

---
+++ GCMChat/app/src/main/java/info/androidhive/gcm/app/EndPoints.java
@@ -0,0 +1,12 @@
+package info.androidhive.gcm.app;
+
+public class EndPoints {
+
+    // localhost url - 
+    public static final String BASE_URL = "http://192.168.43.65/gcm_chat/v1";
+    public static final String LOGIN = BASE_URL + "/user/login";
+    public static final String USER = BASE_URL + "/user/_ID_";
+    public static final String CHAT_ROOMS = BASE_URL + "/chat_rooms";
+    public static final String CHAT_THREAD = BASE_URL + "/chat_rooms/_ID_";
+    public static final String CHAT_ROOM_MESSAGE = BASE_URL + "/chat_rooms/_ID_/message";
+}
---
+++ server side/gcm_chat/include/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gcm_chat');
+ 
+define("GOOGLE_API_KEY", ""); //Place your GOOGLE_API_KEY
+ 
+// push notification flags
+define('PUSH_FLAG_CHATROOM', 1);
+define('PUSH_FLAG_USER', 2);
+ 
+?>
